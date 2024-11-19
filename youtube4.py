77f85724161985c1ac02f52c25d55fed430f5900
M1113 <matthewcornthwaite11@gmail.com>
2024-11-19 21:45:25 +0800

    Managed to print channelId and title

--- youtube4.py
+++ youtube4.py
@@ -14,9 +14,17 @@ def chan_search():
     maxResults=2
     )
     search_response = search_request.execute()
-    #figure how to print search responses
-    for i in search_response:
-        print(search_response[i])
+    search_list = search_response['items']
+    print()
+    #print(search_response['items'][0]['snippet']['channelId'])
+    #print(search_response['items'][0]['snippet']['title'])
+        #snippet = i.get('snippet', {})
+        #print(snippet)
+    
+    for i in search_list:
+        print(i['snippet']['channelId'])
+        print(i['snippet']['title'])
+        print()
 
 
 chan_search()
